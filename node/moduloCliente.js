9b5c18f8486730c5d4edbc3abb8069484af9eb4b
Alan P. Santos <alan.pinheirodossantos@gmail.com>
2022-03-25 07:32:58 +0000

    modulodeterceiros

--- node/moduloCliente.js
+++ node/moduloCliente.js
@@ -2,3 +2,7 @@ const moduleA = require ('./moduloA')
 const moduleB = require ('./moduloB')
 
 console.log(moduleA.ola)
+console.log(moduleA.bemVindo)
+console.log(moduleA.novoExemplo)
+console.log(moduleB.bomDia)
+console.log(moduleB.saudacao())
